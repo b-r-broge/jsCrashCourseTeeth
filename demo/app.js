d6931c4a0b7a41f8a6cee2eaf705dedc5145e875
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-03-21 21:52:25 -0400

    Added if/else statement to count clicks

--- demo/app.js
+++ demo/app.js
@@ -1,28 +1,41 @@
-//get element by id works BUT NOT GET ELEMENT BY CLASS WTF. will not detect 'teeth' if it's a class
-
+var count = 0;
 
 //create trigger event.
 document.getElementById('coffeeBtn').addEventListener('click', function coffeeTime () {
-  console.log('coffee consumed');
-
+  console.log('coffee consumed ' + count);
+  count ++;
   // 'if statement' looks for background color of HTML id 'teeth'
   if (document.getElementById('teeth').style.backgroundColor='#FAF9F4') {
 
+    //code executed if teeth background color is white:
+     (document.getElementById('teeth').style.backgroundColor='#E7E1AD');
+    } else {
 
-
-   (document.getElementById('teeth').style.backgroundColor='#E7E1AD');
-  } else {
-
-    //can leave 'else' part blank
   }
 });
 
 
 // same thing for clicking 'teethCleaning button'
 document.getElementById('teethCleaning').addEventListener('click', function cleaningTime () {
-  console.log('dentist appt');
+  console.log('brushed those pearly whites');
   if (document.getElementById('teeth').style.backgroundColor='#E7E1AD') {
-   (document.getElementById('teeth').style.backgroundColor='#FAF9F4');
-  } else {
-  }
+     (document.getElementById('teeth').style.backgroundColor='#FAF9F4');
+    } else {
+    }
+});
+
+
+//show cavity if clicks >10
+
+var cavity = document.getElementById('cavity');
+
+document.getElementById('coffeeBtn').addEventListener('click', function cavityTime () {
+
+if (count > 6){
+  (cavity.style.backgroundColor='#2C2C2B');
+  alert('Uh oh! Looks like you have a cavity!');
+  console.log('counter more than 6');
+}
+else{
+}
 });
