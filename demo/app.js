496c1b17f693847a1df0b685f853f3392cc63eb0
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-03-22 14:29:41 -0400

    Add greeting function on load

--- demo/app.js
+++ demo/app.js
@@ -4,9 +4,18 @@ var cavity = document.getElementById('cavity');
 
 var teeth = document.getElementById('teeth');
 
+var coffee = document.getElementById('coffeeBtn');
+
+var cleaningMagic = document.getElementById('teethCleaning');
+
+//greeting on load
+function greeting() {
+    alert('Hey! Want some coffee?');
+}
+window.onload = greeting;
 
 //create trigger event.
-document.getElementById('coffeeBtn').addEventListener('click', function coffeeTime() {
+coffee.addEventListener('click', function coffeeTime() {
     console.log('coffee consumed ' + count);
     count++;
     // 'if statement' looks for background color of HTML id 'teeth'
@@ -27,9 +36,11 @@ document.getElementById('coffeeBtn').addEventListener('click', function coffeeTi
 
 
 // same thing for clicking 'teethCleaning button'
-document.getElementById('teethCleaning').addEventListener('click', function cleaningTime() {
+cleaningMagic.addEventListener('click', function cleaningTime() {
     console.log('brushed those pearly whites');
     if (document.getElementById('teeth').style.backgroundColor = '#E7E1AD') {
         (document.getElementById('teeth').style.backgroundColor = '#FAF9F4');
-    } else {}
+    } else {
+
+    }
 });
