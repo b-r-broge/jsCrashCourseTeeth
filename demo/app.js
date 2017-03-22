6c8f3b4af0422f962219a738ad10572a723c44d9
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-03-21 22:03:00 -0400

    Polish

--- demo/app.js
+++ demo/app.js
@@ -6,33 +6,30 @@ var teeth = document.getElementById('teeth');
 
 
 //create trigger event.
-document.getElementById('coffeeBtn').addEventListener('click', function coffeeTime () {
-  console.log('coffee consumed ' + count);
-  count ++;
-  // 'if statement' looks for background color of HTML id 'teeth'
-  if (teeth.style.backgroundColor='#FAF9F4') {
+document.getElementById('coffeeBtn').addEventListener('click', function coffeeTime() {
+    console.log('coffee consumed ' + count);
+    count++;
+    // 'if statement' looks for background color of HTML id 'teeth'
+    if (teeth.style.backgroundColor = '#FAF9F4') {
 
-    //code executed if teeth background color is white:
-     (teeth.style.backgroundColor='#E7E1AD');
+        //code executed if teeth background color is white:
+        (teeth.style.backgroundColor = '#E7E1AD');
     } else {
 
-  }
+    }
 
-  if (count > 6){
-    (cavity.style.backgroundColor='#2C2C2B');
-    alert('Uh oh! Looks like you have a cavity!');
-    console.log('counter more than 6');
-  }
-  else{
-  }
+    if (count > 6) {
+        (cavity.style.backgroundColor = '#2C2C2B');
+        alert('Uh oh! Looks like you have a cavity!');
+        console.log('counter more than 6');
+    } else {}
 });
 
 
 // same thing for clicking 'teethCleaning button'
-document.getElementById('teethCleaning').addEventListener('click', function cleaningTime () {
-  console.log('brushed those pearly whites');
-  if (document.getElementById('teeth').style.backgroundColor='#E7E1AD') {
-     (document.getElementById('teeth').style.backgroundColor='#FAF9F4');
-    } else {
-    }
+document.getElementById('teethCleaning').addEventListener('click', function cleaningTime() {
+    console.log('brushed those pearly whites');
+    if (document.getElementById('teeth').style.backgroundColor = '#E7E1AD') {
+        (document.getElementById('teeth').style.backgroundColor = '#FAF9F4');
+    } else {}
 });
