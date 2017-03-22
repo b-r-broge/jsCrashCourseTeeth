c41eb566924bf1b51b9f1b1693916a2530ea550e
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-03-21 22:01:02 -0400

    Condense js

--- demo/app.js
+++ demo/app.js
@@ -1,16 +1,29 @@
 var count = 0;
 
+var cavity = document.getElementById('cavity');
+
+var teeth = document.getElementById('teeth');
+
+
 //create trigger event.
 document.getElementById('coffeeBtn').addEventListener('click', function coffeeTime () {
   console.log('coffee consumed ' + count);
   count ++;
   // 'if statement' looks for background color of HTML id 'teeth'
-  if (document.getElementById('teeth').style.backgroundColor='#FAF9F4') {
+  if (teeth.style.backgroundColor='#FAF9F4') {
 
     //code executed if teeth background color is white:
-     (document.getElementById('teeth').style.backgroundColor='#E7E1AD');
+     (teeth.style.backgroundColor='#E7E1AD');
     } else {
 
+  }
+
+  if (count > 6){
+    (cavity.style.backgroundColor='#2C2C2B');
+    alert('Uh oh! Looks like you have a cavity!');
+    console.log('counter more than 6');
+  }
+  else{
   }
 });
 
@@ -22,20 +35,4 @@ document.getElementById('teethCleaning').addEventListener('click', function clea
      (document.getElementById('teeth').style.backgroundColor='#FAF9F4');
     } else {
     }
-});
-
-
-//show cavity if clicks >10
-
-var cavity = document.getElementById('cavity');
-
-document.getElementById('coffeeBtn').addEventListener('click', function cavityTime () {
-
-if (count > 6){
-  (cavity.style.backgroundColor='#2C2C2B');
-  alert('Uh oh! Looks like you have a cavity!');
-  console.log('counter more than 6');
-}
-else{
-}
 });
